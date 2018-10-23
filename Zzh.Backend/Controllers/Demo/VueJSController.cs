c5f61b921858e8c61cb56f14d58ed34257a82fb6
zzh <865729986@qq.com>
2018-10-23 17:34:01 +0800

    VueJS练习

--- Zzh.Backend/Controllers/Demo/VueJSController.cs
+++ Zzh.Backend/Controllers/Demo/VueJSController.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Zzh.Model.DB;
+using Zzh.Lib.DB.Repositorys;
+using System.Threading.Tasks;
 
 namespace Zzh.Backend.Controllers.Demo
 {
     public class VueJSController : Controller
     {
         // GET: VueJS
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
             return View();
+        }
+        public async Task<JsonResult> GetUserList()
+        {
+            using (Sys_UserRepository repo = new Sys_UserRepository())
+            {
+                var result = await repo.GetListAsync(1, 100, "");
+                var json= Json(new { total = result.Item1, rows = result.Item2 });
+                return json;
+            }
         }
     }
 }
